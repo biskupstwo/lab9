7aacee43b330fdec7d629304042a0944c19b3bbc
Jan Biskupski <jbiskupski0@outlook.com>
2021-12-16 23:20:47 +0100

    asds

--- script.ts
+++ script.ts
@@ -6,18 +6,15 @@ const changestyle1 = () => {
     let link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'style.css';
-    link.id = 'custom-style';
     document.getElementsByTagName("head").item(0).replaceChild(link, oldlink);
     console.log("zmiana stylu na 1");
 }
 
 const changestyle2 = () => {  
     const oldlink = document.getElementsByTagName("link").item(0);
-    //document.getElementsByTagName("link").removeE();
     let link2 = document.createElement('link');
     link2.rel = 'stylesheet';
     link2.href = 'style2.css';
-    link2.id = 'custom-style';
     document.getElementsByTagName("head").item(0).replaceChild(link2, oldlink);
     console.log("zmiana stylu na 2");
 }
