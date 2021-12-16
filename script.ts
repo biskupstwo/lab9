bfa81e2cf6fb2612242aa7247bce311fb4071f85
Jan Biskupski <jbiskupski0@outlook.com>
2021-12-16 23:18:39 +0100

    ffsds

--- script.ts
+++ script.ts
@@ -2,8 +2,8 @@ let Print = () => console.log("podmianka styli.");
 Print();
 
 const changestyle1 = () => {
-    var oldlink = document.getElementsByTagName("link").item(0);
-    const link = document.createElement('link');
+    const oldlink = document.getElementsByTagName("link").item(0);
+    let link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'style.css';
     link.id = 'custom-style';
@@ -11,10 +11,10 @@ const changestyle1 = () => {
     console.log("zmiana stylu na 1");
 }
 
-const changestyle2 = () => {
-    var oldlink = document.getElementsByTagName("link").item(0);
+const changestyle2 = () => {  
+    const oldlink = document.getElementsByTagName("link").item(0);
     //document.getElementsByTagName("link").removeE();
-    const link2 = document.createElement('link');
+    let link2 = document.createElement('link');
     link2.rel = 'stylesheet';
     link2.href = 'style2.css';
     link2.id = 'custom-style';
@@ -26,18 +26,18 @@ const changestyle2 = () => {
 
 const generuj = () => {
     const navbarr = document.getElementById("#header");
-    var element = document.createElement("a");
+    const element = document.createElement("a");
     element.id = "alink";
     element.innerHTML = "Strona 1";
     element.addEventListener('click', changestyle1);
     navbarr.appendChild(element);
 
-    var imie = document.createElement("b");
+    let imie = document.createElement("b");
     imie.id = "imie";
     imie.innerHTML = "Jan Biskupski";
     navbarr.appendChild(imie);
 
-    var element2 = document.createElement("a");
+    let element2 = document.createElement("a");
     element2.id = "alink";
     element2.innerHTML = "Strona 2";
     element2.addEventListener('click', changestyle2);
